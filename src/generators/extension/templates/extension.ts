82366dde7eb40411b581234edc30180fd292cea9
Alex Van Camp <vancamp.alex@gmail.com>
2022-11-27 19:30:50 -0600

    style: lint

--- src/generators/extension/templates/extension.ts
+++ src/generators/extension/templates/extension.ts
@@ -1,4 +1,4 @@
-import {NodeCG} from '@nodecg/types'
+import { NodeCG } from '@nodecg/types';
 
 module.exports = function (nodecg: NodeCG.ServerAPI) {
 	nodecg.log.info("Hello, from your bundle's extension!");
