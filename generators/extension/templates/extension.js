a95f5e4987d6879aa253d1123b22e72eb1c37e43
Alex Van Camp <vancamp.alex@gmail.com>
2022-11-27 19:53:00 -0600

    chore: remove build outputs

--- generators/extension/templates/extension.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (nodecg) {
-	nodecg.log.info("Hello, from your bundle's extension!");
-	nodecg.log.info("I'm where you put all your server-side code.");
-	nodecg.log.info(`To edit me, open "${__filename}" in your favorite text editor or IDE.`);
-	nodecg.log.info('You can use any libraries, frameworks, and tools you want. There are no limits.');
-	nodecg.log.info('Visit https://nodecg.dev for full documentation.');
-	nodecg.log.info('Good luck!');
-};
